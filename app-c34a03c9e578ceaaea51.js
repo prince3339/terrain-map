aa5c986f845526fbed79e22d03f5b09dcec01a89
prince3339 <prince3339@ymail.com>
2020-06-08 18:29:39 +0600

    Updates

--- app-c34a03c9e578ceaaea51.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],[],[["UxWs",3,2,0]]]);
-//# sourceMappingURL=app-c34a03c9e578ceaaea51.js.map
---
+++ app-e5a1f4831c16a5a2c614.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],[],[["UxWs",4,1,0]]]);
+//# sourceMappingURL=app-e5a1f4831c16a5a2c614.js.map
